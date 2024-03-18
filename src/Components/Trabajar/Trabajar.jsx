10f72969caad1156e3a6696445ab57d771373c3d
Mauricio Zuñiga <mantonio.zr@gmail.com>
2024-03-18 20:40:47 -0300

    secciones

--- src/Components/Trabajar/Trabajar.jsx
+++ src/Components/Trabajar/Trabajar.jsx
@@ -1,23 +1,43 @@
 import React from 'react'
 import "./Trabajar.css"
+import Tecnologico from "../../image/personaje-tecnologico.png"
+import Csharp from "../../image/csharp.png"
+import Amazon from "../../image/amazon.png"
+import SQL from "../../image/sql-server.png"
+import Serenity from "../../image/serenity.png";
+import Reactjs from "../../image/react.png";
+import TypeScript from "../../image/types.png";
+import Javascript from "../../image/javascript.png";
 
 const Trabajar = () => {
   return (
     <div className='trabajar'>
         <div className='trabajar-info'>
             <div className='trabajar-izq'>
+                <h2>¿Porqué trabajar <br/>con nosotros?</h2>
+                <p>Nuestro enfoque se centra en brindar soluciones innovadoras y personalizadas a nuestros clientes, utilizando las últimas herramientas y tecnologías disponibles.
+
+Con un sólido historial de éxito en la implementación de proyectos tecnológicos, podemos garantizar resultados excepcionales y un enfoque centrado en la excelencia en cada paso del camino.</p>
 
             </div>
             <div className='trabajar-der'>
-
+   <img src={Tecnologico} />
             </div>
 
         </div>
         <div className='trabajar-logos'>
-            <div>
-                <img src="" />
-            </div>
             
+                <img src={Csharp} />
+                <img src={Amazon} />
+                <img src={SQL} />
+                <img src={Serenity} />
+                <img src={Reactjs} />
+                <img src={TypeScript} />
+                <img src={Javascript} />
+        
+
+
+
         </div>
     </div>
   )
