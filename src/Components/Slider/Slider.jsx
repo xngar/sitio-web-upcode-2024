c7cc6bd0545f8c53d646e3ceca96dca80c6e9222
Mauricio Zuñiga <mantonio.zr@gmail.com>
2024-03-21 12:19:23 -0300

    agregando animaciones

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -2,14 +2,46 @@ import React from 'react'
 import "./Slider.css"
 import SliderImg from "../../image/slider-img.png";
 import Up from "../../image/up.png";
+import {motion} from "framer-motion";
 
 const Slider = () => {
   return (
     <div className='slider'>
         <div className='slider-izq'>
-          <h1>Hazle un  <img src={Up} /><br/>a tus proyectos</h1>
-          <p>Transformamo tus proyectos en realidades digitales cautivadoras. Impulsa tu presencia en línea con nuestras soluciones de vanguardia.</p>
-          <button>Contáctanos</button>
+          <motion.h1 
+          initial={{opacity:0, x:-100}}
+          
+          whileInView={{opacity:1, x:0}}
+          
+          transition={{
+            duration:2,
+            type:"spring"
+          }}
+          
+          >Hazle un  <img src={Up} /><br/>a tus proyectos</motion.h1>
+          <motion.p
+          initial={{opacity:0, x:-100}}
+          whileInView={{opacity:1, x:0}}
+          transition={{duration:2,
+            type:"spring",
+          delay:0.5}}
+          
+          >Transformamo tus proyectos en realidades digitales cautivadoras. Impulsa tu presencia en línea con nuestras soluciones de vanguardia.</motion.p>
+          <motion.button
+          initial={{opacity:0}}
+          
+          whileInView={{opacity:1}}
+          whileHover={{
+            scale:1.2,
+            background:"white",
+            color:"orange"
+
+          }}
+          transition={{
+            type:"spring",
+            duration:2
+          }}
+          >Contáctanos</motion.button>
         </div>
         <div className='slider-der'>
           <img src={SliderImg} />
