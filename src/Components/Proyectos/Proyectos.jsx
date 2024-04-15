ea366c759d2c02e0748125998a71aed25d2f2285
Mauricio Zuñiga <mantonio.zr@gmail.com>
2024-04-15 11:54:16 -0400

    agregando proyectos

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -4,33 +4,163 @@ import ImaProy1 from "../../image/imagen-proyecto.jpg";
 import NET from "../../image/csharp.png";
 import TYPES from "../../image/types.png";
 import CLOUD from "../../image/aws.png";
+import AMAZON from "../../image/amazon.png"
+import SQL from "../../image/sql-server.png"
+import REACT from "../../image/react.png"
+import WP from "../../image/WP.png"
 
-
-const Proyectos = ({motion}) => {
+const Proyectos = ({ motion }) => {
   return (
     <div className="proyectos" id="Proyectos">
       <div>
         <h2>Proyectos</h2>
       </div>
       {/* Estructura de cada proyecto */}
+
+
+
+
+      {/* CARD 1 */}
       <motion.div
-      initial={{opacity:0,x:50}}
-      whileInView={{opacity:1, x:0}}
-      transition={{duration:1}}
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="proyecto"
+      >
+        <div className="proyecto-detalle">
+          <div>
+            <h2>Sitio Web - Autoadministrable</h2>
+            <h2>
+              Cliente: <span>Turisclub</span>
+            </h2>
+            <h3 className="titulo-descripcion">Descripcion proyecto</h3>
+            <p>
+              <h3>Creación de una API Rest - Web Services</h3>
+              Para acceder al contenido desde un
+              tercero. Integración pasarela de pago GetNet.
+              
+              <h3>Infraestructura AWS para sus desarrollos</h3>
+              Sitio Web El cliente solicitó un sitio web
+              para la exposición y venta de paquetes turísticos, realizando el
+              mantenimiento de los productos desde su ABM de contenido. Api Rest
+              EL cliente solicitó el desarrollo de un Web Service (API), formato
+              Json, para que los clientes puedan consumir los productos
+              publicados en la web. Integración GetNet: se integró pasarela de
+              pago GetNet (Santander), para que los clientes puedan pagar sus
+              reservas desde cualquier lugar.
+            </p>
+          </div>
+          <div className="tecnologias">
+            <div className="tec1">
+              <img src={NET} />
+            </div>
+            <div className="tec1">
+              <img src={REACT} />
+            </div>
+            <div className="tec1">
+              <img src={CLOUD} />
+            </div>
+          </div>
+        </div>
+        <div className="proyecto-imagen">
+          <img src={ImaProy1} />
+        </div>
+      </motion.div>
+
+     {/* CARD 2 */}
+
+
+     <motion.div
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="proyecto"
+      >
+        <div className="proyecto-detalle">
+          <div>
+            <h2>Sistema Gestión</h2>
+            <h2>
+              Cliente: <span>Agropraxis Group Ltda</span>
+            </h2>
+            <h3 className="titulo-descripcion">Descripcion proyecto</h3>
+            <p>
+            El Cliente solicite un desarrollo de CRM gestión de procedimientos y análisis de Predios agrícolas, previa certificación de Servicio Agrícola y Ganadero (SAG).
+
+Plataforma Acceso Clientes para ver sus resultados de análisis en línea y pudiendo obtener su certificado.
+            </p>
+          </div>
+          <div className="tecnologias">
+            <div className="tec1">
+              <img src={NET} />
+            </div>
+            <div className="tec1">
+              <img src={REACT} />
+            </div>
+            <div className="tec1">
+              <img src={CLOUD} />
+            </div>
+
+            <div className="tec1">
+              <img src={SQL} />
+            </div>
+          </div>
+        </div>
+        <div className="proyecto-imagen">
+          <img src={ImaProy1} />
+        </div>
+      </motion.div>
+
+      {/* card 3 */}
+
+      <motion.div
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="proyecto"
+      >
+        <div className="proyecto-detalle">
+          <div>
+            <h2>Sitio Web Autoadministrable</h2>
+            <h2>
+              Cliente: <span>Broker Plus</span>
+            </h2>
+            <h3 className="titulo-descripcion">Descripcion proyecto</h3>
+            <p>
+            Cliente solicitó un desarrollo de un sitio web autoadministrable, para la venta y arriendo de propiedades.
+            </p>
+          </div>
+          <div className="tecnologias">
+            <div className="tec1">
+              <img src={WP} />
+            </div>
+            
+            
+          </div>
+        </div>
+        <div className="proyecto-imagen">
+          <img src={ImaProy1} />
+        </div>
+      </motion.div>
+
+      {/* card 4  */}      
       
-      className="proyecto">
+      <motion.div
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="proyecto"
+      >
         <div className="proyecto-detalle">
           <div>
-            <h2>Desarrollo Web</h2>
-            <h3>
-              Cliente: <span>Construcora Hurue</span>
-            </h3>
-            <h3>Descripcion proyecto</h3>
+            <h2>CRM Gestión de Socios</h2>
+            <h2>
+              Cliente: <span>Sindicato Logística Walmart</span>
+            </h2>
+            <h3 className="titulo-descripcion">Descripcion proyecto</h3>
             <p>
-              una empresa que se dedica a la ejecución de proyectos públicos y
-              privados, principalmente contratos del sector público en la
-              Dirección de Obras Hidráulicas, del Ministerio de Obras Públicas y
-              también proyectos con Municipalidades en el país.{" "}
+            Cliente solicitó un CRM de gestión de Socios afiliados al Sindicato. 
+Se le otorga beneficios al Socio como a las cargas que este posea. Se integra Infraestructura AWS de alojamiento para la aplicación desarrollada.
+
             </p>
           </div>
           <div className="tecnologias">
@@ -38,7 +168,7 @@ const Proyectos = ({motion}) => {
               <img src={NET} />
             </div>
             <div className="tec1">
-              <img src={TYPES} />
+              <img src={SQL} />
             </div>
             <div className="tec1">
               <img src={CLOUD} />
@@ -50,33 +180,28 @@ const Proyectos = ({motion}) => {
         </div>
       </motion.div>
 
-
+      {/* card 5 */}
 
       <motion.div
-      initial={{opacity:0,x:50}}
-      whileInView={{opacity:1, x:0}}
-      transition={{duration:1}}
-      className="proyecto">
+        initial={{ opacity: 0, x: 50 }}
+        whileInView={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+        className="proyecto"
+      >
         <div className="proyecto-detalle">
           <div>
-            <h2>Desarrollo Web</h2>
-            <h3>
-              Cliente: <span>Construcora Hurue</span>
-            </h3>
-            <h3>Descripcion proyecto</h3>
+            <h2>Servicios de Alojamiento</h2>
+            <h2>
+              Cliente: <span>Travelaway</span>
+            </h2>
+            <h3 className="titulo-descripcion">Descripcion proyecto</h3>
             <p>
-              una empresa que se dedica a la ejecución de proyectos públicos y
-              privados, principalmente contratos del sector público en la
-              Dirección de Obras Hidráulicas, del Ministerio de Obras Públicas y
-              también proyectos con Municipalidades en el país.{" "}
+            Cliente solicitó servicio de alojamiento AWS para sus CRM de terceros.
             </p>
           </div>
           <div className="tecnologias">
             <div className="tec1">
-              <img src={NET} />
-            </div>
-            <div className="tec1">
-              <img src={TYPES} />
+              <img src={SQL} />
             </div>
             <div className="tec1">
               <img src={CLOUD} />
@@ -87,8 +212,7 @@ const Proyectos = ({motion}) => {
           <img src={ImaProy1} />
         </div>
       </motion.div>
-
-
+      
     </div>
   );
 };
