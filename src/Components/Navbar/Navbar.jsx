9281f2933342e02fd67f17217b11a064b5d7e9a6
Mauricio Zuñiga <mantonio.zr@gmail.com>
2024-04-18 15:25:39 -0400

    menu ham 2

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,8 @@ import { Link } from "react-scroll";
 import Proyectos from "../Proyectos/Proyectos";
 import { AnimatePresence } from "framer-motion";
 import { SlMenu } from "react-icons/sl";
+import { AiOutlineCloseSquare } from "react-icons/ai";
+
 
 const Navbar = ({ motion }) => {
 
@@ -15,9 +17,16 @@ const Navbar = ({ motion }) => {
     const fnCambiar =(valor,num)=>{
         setOpen(valor)
         setOpenValor(num);
+        
 
 
     }
+
+
+    const fnMenu = (valor)=>{
+     setMenuStatus(valor);
+    }
+   
   return (
     <div className="navbar" id="navbar">
       <header>
@@ -31,13 +40,11 @@ const Navbar = ({ motion }) => {
               whileInView={{ rotate: [0,10,0,0] }}
               transition={{repeat:2, duration:1}}
               src={logo}
+              
             />
           </AnimatePresence>
         </div>
-        <div >
-          
-        <SlMenu className="btnMenuHam" onClick={()=>setMenuStatus(true)}  style={{color:'white', fontSize:'40', cursor:'pointer', marginLeft:'50'}} />
-        </div>
+               
         <div>
           <motion.nav
             initial={{ opacity: 0, x: 100 }}
@@ -123,7 +130,9 @@ const Navbar = ({ motion }) => {
 
       {/* MENU HAMBURGUESA MOVIL */}
       {/* ---------------------------- */}
-
+      <SlMenu className="btnMenuHam"  onClick={()=> fnMenu(true)}   />
+      
+        {/* 
         <div className="menuBurger" style={{opacity: menuStatus ? 100:0}}>
         
         <ul>
@@ -196,8 +205,89 @@ const Navbar = ({ motion }) => {
                 <li>Contáctenos</li>
               </Link>
             </ul>
-      </div>
+      </div> */}
       </header>
+      <div className="menuBurger"  style={{display:menuStatus ? 'block' :'none'}}>
+      <AiOutlineCloseSquare onClick={()=>fnMenu(false)} style={{fontSize:'40'}} />
+      <ul>  
+              <Link spy={true} to="Somos" smooth={true}>
+                <li
+                onMouseEnter={()=>fnCambiar(true,1)}
+                onMouseLeave={()=>setOpen(false)}
+                onClick={()=>setMenuStatus(false)}
+                >
+                  ¿Quiénes Somos?
+                  <span
+                    style={{
+                      width: "140px",
+                      height: "3px",
+                      backgroundColor: "#f18c1a",
+                      position: "absolute",
+                      transform: open && openValor ===1? "scaleX(1)" : "scaleX(0)",
+                      top:'60px',
+                      left:'0px',
+                      transformOrigin:'left',
+                      transition:"transform .2s ease"
+                    }}
+                  />
+                </li>
+              </Link>
+              <Link spy={true} to="Hacemos" smooth={true}>
+                <li
+                onMouseEnter={()=>fnCambiar(true,2)}
+                onMouseLeave={()=>setOpen(false)}
+                onClick={()=>setMenuStatus(false)}
+                >¿Qué hacemos?
+                <span
+                    style={{
+                      width: "125px",
+                      height: "3px",
+                      backgroundColor: "#f18c1a",
+                      position: "absolute",
+                      transform: open && openValor ===2 ? "scaleX(1)" : "scaleX(0)",
+                      top:'60px',
+                      left:'160px',
+                      transformOrigin:'left',
+                      transition:"transform .2s ease"
+                    }}
+                  />
+                </li>
+              </Link>
+              <Link spy={true} to="Vision" smooth={true}>
+                <li
+                onMouseEnter={()=>fnCambiar(true,3)}
+                onMouseLeave={()=>setOpen(false)}
+                onClick={()=>setMenuStatus(false)}
+                >Nuestra visión
+                <span
+                    style={{
+                      width: "115px",
+                      height: "3px",
+                      backgroundColor: "#f18c1a",
+                      position: "absolute",
+                      transform: open && openValor ===3 ? "scaleX(1)" : "scaleX(0)",
+                      top:'60px',
+                      left:'310px',
+                      transformOrigin:'left',
+                      transition:"transform .2s ease"
+                    }}
+                  />
+                </li>
+              </Link>
+              <Link spy={true} to="Proyectos" smooth={true}>
+                {" "}
+                <li
+                onClick={()=>setMenuStatus(false)}
+                >Proyectos</li>
+              </Link>
+              <Link spy={true} to="Contactenos" smooth={true}>
+                <li
+                onClick={()=>setMenuStatus(false)}>Contáctenos</li>
+              </Link>
+            </ul>
+              
+      </div>
+      
     </div>
   );
 };
