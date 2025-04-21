14dbbc051189049aeb478474316cf86f4923ab3c
xngar <mantonio.zr@gmail.com>
2025-04-21 13:28:26 -0400

    prueba desde cursor

--- src/Components/Somos/Somos.jsx
+++ src/Components/Somos/Somos.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 import "./Somos.css"
 
 
-const Somos = ({motion}) => {
+const Somos = ({ motion }) => {
   return (
     <div className='somos' id='Somos'>
-        <motion.div
-        initial={{opacity:0,y:100}}
-        whileInView={{opacity:1, y:0}}
-        transition={{duration:1}}
-        >
-            <h2>¿Quiénes Somos?</h2>
-        </motion.div>
-        <motion.div
-        initial={{opacity:0,y:100}}
-        whileInView={{opacity:1, y:0}}
-        transition={{duration:1}}
-        >
-            <p>Somos una empresa joven y dinámica con una visión fresca y enfoque innovador. Nuestras prácticas sólidas y nuestras ideas vanguardistas garantizan un rendimiento excepcional para tu web o aplicación. Nos comprometemos a ser tu socio tecnológico de confianza, acompañándote en cada etapa de la evolución de la industria y brindándote soluciones estratégicas a corto, mediano y largo plazo. </p>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h2>¿Quiénes Somos?</h2>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <p>-Somos una empresa joven y dinámica con una visión fresca y enfoque innovador. Nuestras prácticas sólidas y nuestras ideas vanguardistas garantizan un rendimiento excepcional para tu web o aplicación. Nos comprometemos a ser tu socio tecnológico de confianza, acompañándote en cada etapa de la evolución de la industria y brindándote soluciones estratégicas a corto, mediano y largo plazo. </p>
 
-        </motion.div>
+      </motion.div>
     </div>
   )
 }
